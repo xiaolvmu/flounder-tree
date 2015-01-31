5265cd6056c75f7ac59353ca5a0ae9905bdd57fc
HashBang <hashbang173@gmail.com>
2015-01-31 14:21:36 -0500

    flounder: Set SELinux to Permissive
    Change-Id: I42fbbbe4b06db094416803766f03bbb9df737635

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,7 +45,7 @@ KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/darwin-x86/aarch64/aarch6
 endif
 KERNEL_TOOLCHAIN_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_SOURCE := kernel/htc/flounder
-TARGET_KERNEL_CONFIG := floundertest_defconfig
+TARGET_KERNEL_CONFIG := flounder_defconfig
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 
