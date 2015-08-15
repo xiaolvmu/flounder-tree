c4748f120607cfc2db15d42f33809f0482c8879c
Simon Sickle <simon@simonsickle.com>
2015-08-15 16:52:25 -0700

    Update flounder to 5.1.1_r13
    Change-Id: Ib2bdb8de515736042c71012ce58f1d1844737628
    Signed-off-by: Simon Sickle <simon@simonsickle.com>

--- cm.mk
+++ cm.mk
@@ -1,6 +1,7 @@
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2048
 TARGET_SCREEN_WIDTH := 1536
+TARGET_PREBUILT_KERNEL := false
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
@@ -13,10 +14,14 @@ $(call inherit-product, device/htc/flounder/aosp_flounder.mk)
 
 $(call inherit-product-if-exists, vendor/htc/flounder/device-vendor.mk)
 
+# Enable USB OTG (CAF commit to Settings)
+ADDITIONAL_BUILD_PROPERTIES += \
+    persist.sys.isUsbOtgEnabled=true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=flounder \
-    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY47X/1849464:user/release-keys \
-    PRIVATE_BUILD_DESC="flounder-user 5.1.1 LMY47X 1849464 release-keys"
+    BUILD_FINGERPRINT=google/volantis/flounder:5.1.1/LMY48I/2074855:user/release-keys \
+    PRIVATE_BUILD_DESC="volantis-user 5.1.1 LMY48I 2074855 release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := flounder
--- device.mk
+++ device.mk
@@ -20,6 +20,16 @@ PRODUCT_PACKAGES := \
     wpa_supplicant \
     wpa_supplicant.conf
 
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+ifeq ($(USE_SVELTE_KERNEL), true)
+LOCAL_KERNEL := device/htc/flounder_svelte-kernel/Image.gz-dtb
+else
+LOCAL_KERNEL := device/htc/flounder-kernel/Image.gz-dtb
+endif # USE_SVELTE_KERNEL
+else
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
 
 # This ensures the needed build tools are available.
@@ -243,10 +253,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=volantis \
     BUILD_FINGERPRINT="google/volantis/flounder:5.1.1/LMY47X/1849464:user/release-keys" \
     PRIVATE_BUILD_DESC="volantis-user 5.1.1 LMY47X 1849464 release-keys"
-
-# Enable USB OTG (CAF commit to Settings)
-ADDITIONAL_BUILD_PROPERTIES += \
-    persist.sys.isUsbOtgEnabled=true
 
 $(call inherit-product-if-exists, hardware/nvidia/tegra132/tegra132.mk)
 $(call inherit-product-if-exists, vendor/nvidia/proprietary-tegra132/tegra132-vendor.mk)
